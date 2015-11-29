c8cfc1691cd79b99f5cbce8eb0990186804d2053
HOHD <sss>
2015-11-29 19:07:34 +0800

    首页 radiobutton，viewpager 互相联动

--- src/com/hohd/interest/MainActivity.java
+++ src/com/hohd/interest/MainActivity.java
@@ -7,15 +7,19 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 import com.hohd.interest.adapter.HomeContentAdapter;
 import com.hohd.interest.fragment.ContentFragment;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements OnCheckedChangeListener, OnPageChangeListener {
 	ViewPager vpContent;
-
+	RadioGroup rgTitle;
 	List<Fragment> list;
+	int[] ids = { R.id.radio0, R.id.radio1, R.id.radio2, R.id.radio3, R.id.radio4 };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +29,18 @@ public class MainActivity extends FragmentActivity {
 
 		initView();
 	}
-	public void click(View view){
-		
+
+	public void click(View view) {
+
 	}
 
 	private void initView() {
 		// initView
 		vpContent = (ViewPager) findViewById(R.id.vpContent);
+		vpContent.setOnPageChangeListener(this);
+
+		rgTitle = (RadioGroup) findViewById(R.id.rgTitle);
+		rgTitle.setOnCheckedChangeListener(this);
 
 		for (int i = 0; i < 5; i++) {
 			ContentFragment fragment = ContentFragment.create(i);
@@ -42,6 +51,49 @@ public class MainActivity extends FragmentActivity {
 		HomeContentAdapter adapter = new HomeContentAdapter(getSupportFragmentManager(), list);
 
 		vpContent.setAdapter(adapter);
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		switch (checkedId) {
+		case R.id.radio0:// 最新
+			vpContent.setCurrentItem(0);
+			break;
+		case R.id.radio1:// 热门
+
+			vpContent.setCurrentItem(1);
+			break;
+		case R.id.radio2:// 娱乐
+
+			vpContent.setCurrentItem(2);
+			break;
+		case R.id.radio3:// 生活
+
+			vpContent.setCurrentItem(3);
+			break;
+		case R.id.radio4:// 科技
+			vpContent.setCurrentItem(4);
+
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+	}
+
+	@Override
+	public void onPageSelected(int arg0) {
+		rgTitle.check(ids[arg0]);
 	}
 
 }
